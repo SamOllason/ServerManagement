c1979227b7499ac018186513b97054362ff6e0ce
Sam Ollason <sam.ollason@aplan.co.uk>
2024-10-30 13:09:19 +0000

    Add project files.

---
+++ Components/Shared/ServerComponent.cs
@@ -0,0 +1,10 @@
+﻿using ServerManagement.Models;
+
+namespace ServerManagement.Components.Shared;
+public partial class ServerComponent
+{
+	private string status = "Server is offline";
+
+	private Server server = new();
+
+}
